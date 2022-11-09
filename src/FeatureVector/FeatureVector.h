49203cc3481a2e094388ed67b7a08c3cf5ac501d
MartinNose <liucmartin@qq.com>
2022-11-08 23:10:27 -0800

    add FeatureVector implementation

--- src/FeatureVector/FeatureVector.h
+++ src/FeatureVector/FeatureVector.h
@@ -1,35 +1,76 @@
 #pragma once
 
+#include <iostream>
+#include <cstdlib>
+#include "global.h"
+
 template<typename Scalar = float>
 class FeatureVector {
 public:
-    void initialize(size_t width, size_t height, size_t spp);
+    void initialize(size_t _width, size_t _height, size_t _spp) {
+        width = _width;
+        height = _height;
+        spp = _spp;
+        isInitialized = true;
 
-    Scalar getPosition(size_t x, size_t y, size_t k, OFFSET offset);
-    Scalar getColor(size_t x, size_t y, size_t k, OFFSET offset);
-    Scalar getFeature(size_t x, size_t y, size_t k, OFFSET offset);
-    Scalar getRandomParameter(size_t x, size_t y, size_t k, OFFSET offset);
 
-	Scalar setPosition(size_t x, size_t y, size_t k, Scalar position, OFFSET offset);
-	Scalar setColor(size_t x, size_t y, size_t k, Scalar color, OFFSET offset);
-	Scalar setFeature(size_t x, size_t y, size_t k, Scalar* features, OFFSET offset, size_t size);
-	Scalar setFeature(size_t index, Scalar feature);
-	Scalar setRandomParameter(size_t x, size_t y, size_t k, Scalar randomParameter, OFFSET offset);
+        randomParameters = new Scalar[width * height * spp * 28 * sizeof(Scalar)];
+        if (randomParameters == NULL) {
+            std::cerr << "Failed to allocate memory" << std::endl;
+        }
+    }
 
-    void setWidth(size_t width);
-    void setHeight(size_t height);
-    void setSamplesPerPixel(size_t samplesPerPixel);
+    static Scalar getPosition(size_t x, size_t y, size_t k, OFFSET offset) {
+        return randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + offset];
+    } 
+    static Scalar getColor(size_t x, size_t y, size_t k, OFFSET offset) {
+        return randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + offset];
+    }
+    static Scalar getFeature(size_t x, size_t y, size_t k, OFFSET offset) {
+        return randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + offset];
+    }
+    static Scalar getRandomParameter(size_t x, size_t y, size_t k, OFFSET offset) {
+        return randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + offset];
+    }
+
+	static Scalar setPosition(size_t x, size_t y, size_t k, Scalar* positions) {
+        randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + WORLD_1_X] = positions[0];
+        randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + WORLD_1_Y] = positions[1];
+        randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + WORLD_1_Z] = positions[2];
+    }
+	static Scalar setColor(size_t x, size_t y, size_t k, Scalar* colors) {
+        randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + COLOR_1] = colors[0];
+        randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + COLOR_2] = colors[1];
+        randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + COLOR_3] = colors[2];
+    }
+	// static Scalar setFeature(size_t x, size_t y, size_t k, Scalar* features) {
+    //     randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + offset] = ;
+    // }
+	static Scalar setRandomParameter(size_t x, size_t y, size_t k, OFFSET offset, Scalar para) {
+        randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + offset] = para;
+    }
+
+    // static void setWidth(size_t width);
+    // static void setHeight(size_t height);
+    // static void setSamplesPerPixel(size_t samplesPerPixel);
 
 private:
-    FeatureVector();
-    ~FeatureVector();
+    FeatureVector() {
+        isInitialized = false;
+    }
+    ~FeatureVector() {
+        if (isInitialized) {
+            delete randomParameters;
+        }
+    }
 
-    Scalar* positions;
-    Scalar* colors;
-    Scalar* features;
-    Scalar* randomParameters;
+    // static Scalar* positions;
+    // static Scalar* colors;
+    // static Scalar* features;
+    static Scalar* randomParameters;
 
-    size_t width;
-    size_t height;
-    size_t spp;
+    static size_t width;
+    static size_t height;
+    static size_t spp;
+    bool isInitialized;
 };
