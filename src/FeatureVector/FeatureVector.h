d319e5a0282b61d8ce6ea8e541153a62d8950451
MartinNose <liucmartin@qq.com>
2022-11-13 15:12:38 -0800

    add getters during rendering process\n todo add texture getter during all the material class

--- src/FeatureVector/FeatureVector.h
+++ src/FeatureVector/FeatureVector.h
@@ -1,76 +1,51 @@
 #pragma once
 
+#ifndef _SRC_FEATUREVECTOR_
+#define _SRC_FEATUREVECTOR_
+
 #include <iostream>
 #include <cstdlib>
+#include <string>
+#include "pbrt.h"
+#include "geometry.h"
 #include "global.h"
 
-template<typename Scalar = float>
+using namespace pbrt;
 class FeatureVector {
-public:
-    void initialize(size_t _width, size_t _height, size_t _spp) {
-        width = _width;
-        height = _height;
-        spp = _spp;
-        isInitialized = true;
-
-
-        randomParameters = new Scalar[width * height * spp * 28 * sizeof(Scalar)];
-        if (randomParameters == NULL) {
-            std::cerr << "Failed to allocate memory" << std::endl;
-        }
-    }
-
-    static Scalar getPosition(size_t x, size_t y, size_t k, OFFSET offset) {
-        return randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + offset];
-    } 
-    static Scalar getColor(size_t x, size_t y, size_t k, OFFSET offset) {
-        return randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + offset];
-    }
-    static Scalar getFeature(size_t x, size_t y, size_t k, OFFSET offset) {
-        return randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + offset];
-    }
-    static Scalar getRandomParameter(size_t x, size_t y, size_t k, OFFSET offset) {
-        return randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + offset];
-    }
-
-	static Scalar setPosition(size_t x, size_t y, size_t k, Scalar* positions) {
-        randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + WORLD_1_X] = positions[0];
-        randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + WORLD_1_Y] = positions[1];
-        randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + WORLD_1_Z] = positions[2];
-    }
-	static Scalar setColor(size_t x, size_t y, size_t k, Scalar* colors) {
-        randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + COLOR_1] = colors[0];
-        randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + COLOR_2] = colors[1];
-        randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + COLOR_3] = colors[2];
-    }
-	// static Scalar setFeature(size_t x, size_t y, size_t k, Scalar* features) {
-    //     randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + offset] = ;
-    // }
-	static Scalar setRandomParameter(size_t x, size_t y, size_t k, OFFSET offset, Scalar para) {
-        randomParameters[y * width * spp* 28 + x * spp * 28 + k * 28 + offset] = para;
-    }
-
-    // static void setWidth(size_t width);
-    // static void setHeight(size_t height);
-    // static void setSamplesPerPixel(size_t samplesPerPixel);
-
 private:
-    FeatureVector() {
-        isInitialized = false;
-    }
-    ~FeatureVector() {
-        if (isInitialized) {
-            delete randomParameters;
-        }
-    }
-
-    // static Scalar* positions;
-    // static Scalar* colors;
-    // static Scalar* features;
-    static Scalar* randomParameters;
+    // static float* positions;
+    // static float* colors;
+    // static float* features;
+    static float* randomParameters;
 
     static size_t width;
     static size_t height;
     static size_t spp;
-    bool isInitialized;
+    static bool isInitialized;
+public:
+    FeatureVector();
+    ~FeatureVector();
+
+    static void initialize(size_t _width, size_t _height, size_t _spp);
+    static float getPosition(size_t x, size_t y, size_t k, OFFSET offset);
+    static float getColor(size_t x, size_t y, size_t k, OFFSET offset);
+    static float getFeature(size_t x, size_t y, size_t k, OFFSET offset);
+    static float getRandomParameter(size_t x, size_t y, size_t k, OFFSET offset);
+
+	static void setPosition(size_t x, size_t y, size_t k, float* positions);
+	static void setPosition(size_t x, size_t y, size_t k, Point3f &position);
+	static void setNormal(size_t x, size_t y, size_t k, Point3f &normal);
+	static void setColor(size_t x, size_t y, size_t k, float* colors);
+    static void setTexture(size_t x, size_t y, size_t k, Spectrum r);
+	// static void setColor(size_t x, size_t y, size_t k, float* colors);
+	// static float setFeature(size_t x, size_t y, size_t k, float* features);
+	static void setRandomParameter(size_t x, size_t y, size_t k, OFFSET offset, float para);
+
+    static void write_dat();
+
+    // static void setWidth(size_t width);
+    // static void setHeight(size_t height);
+    // static void setSamplesPerPixel(size_t samplesPerPixel);
 };
+
+#endif
