331a96cd9c256e3074822bedb79cae67f2226814
MartinNose <liucmartin@qq.com>
2022-11-13 23:27:16 -0800

    fix featureVector bug

--- src/FeatureVector/FeatureVector.cpp
+++ src/FeatureVector/FeatureVector.cpp
@@ -36,47 +36,51 @@ void FeatureVector::initialize(size_t _width, size_t _height, size_t _spp) {
 }
 
 float FeatureVector::getPosition(size_t x, size_t y, size_t k, OFFSET offset) {
-    return randomParameters[y * width * spp *  + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + offset];
+    return randomParameters[y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + offset];
 } 
 float FeatureVector::getColor(size_t x, size_t y, size_t k, OFFSET offset) {
-    return randomParameters[y * width * spp *  + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + offset];
+    return randomParameters[y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + offset];
 }
 float FeatureVector::getFeature(size_t x, size_t y, size_t k, OFFSET offset) {
-    return randomParameters[y * width * spp *  + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + offset];
+    return randomParameters[y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + offset];
 }
 float FeatureVector::getRandomParameter(size_t x, size_t y, size_t k, OFFSET offset) {
-    return randomParameters[y * width * spp *  + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + offset];
+    return randomParameters[y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + offset];
 }
 
 void FeatureVector::setPosition(size_t x, size_t y, size_t k, float* positions) {
-    randomParameters[y * width * spp *  + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + WORLD_1_X] = positions[0];
-    randomParameters[y * width * spp *  + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + WORLD_1_Y] = positions[1];
-    randomParameters[y * width * spp *  + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + WORLD_1_Z] = positions[2];
+    randomParameters[y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + WORLD_1_X] = positions[0];
+    randomParameters[y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + WORLD_1_Y] = positions[1];
+    randomParameters[y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + WORLD_1_Z] = positions[2];
 }
 void FeatureVector::setColor(size_t x, size_t y, size_t k, float* colors) {
-    randomParameters[y * width * spp *  + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + COLOR_1] = colors[0];
-    randomParameters[y * width * spp *  + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + COLOR_2] = colors[1];
-    randomParameters[y * width * spp *  + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + COLOR_3] = colors[2];
+    randomParameters[y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + COLOR_1] = colors[0];
+    randomParameters[y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + COLOR_2] = colors[1];
+    randomParameters[y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + COLOR_3] = colors[2];
 }
 
 void FeatureVector::setPosition(size_t x, size_t y, size_t k, Point3f &position) {
-    randomParameters[y * width * spp *  + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + WORLD_1_X] = position.x;
-    randomParameters[y * width * spp *  + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + WORLD_1_Y] = position.y;
-    randomParameters[y * width * spp *  + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + WORLD_1_Z] = position.z;
+    randomParameters[y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + WORLD_1_X] = position.x;
+    randomParameters[y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + WORLD_1_Y] = position.y;
+    randomParameters[y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + WORLD_1_Z] = position.z;
 
 }
 void FeatureVector::setNormal(size_t x, size_t y, size_t k, Normal3f &normal) {
-    randomParameters[y * width * spp *  + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + NORM_1_X] = normal.x;
-    randomParameters[y * width * spp *  + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + NORM_1_Y] = normal.y;
-    randomParameters[y * width * spp *  + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + NORM_1_Z] = normal.z;
+    randomParameters[y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + NORM_1_X] = normal.x;
+    randomParameters[y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + NORM_1_Y] = normal.y;
+    randomParameters[y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + NORM_1_Z] = normal.z;
 
 }
 void FeatureVector::setTexture(size_t x, size_t y, size_t k, Spectrum r) {
-    r.ToRGB(randomParameters + (y * width * spp *  + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + TEXTURE_1_X));
+    r.ToRGB(randomParameters + (y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + TEXTURE_1_X));
+}
+
+void FeatureVector::setColor(size_t x, size_t y, size_t k, Spectrum &R) {
+    R.ToRGB(randomParameters + (y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + COLOR_1));
 }
 
 // static float setFeature(size_t x, size_t y, size_t k, Scalar* features) {
-//     randomParameters[y * width * spp *  + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + offset] = ;
+//     randomParameters[y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + offset] = ;
 // }
 void FeatureVector::setRandomParameter(size_t x, size_t y, size_t k, OFFSET offset, float para) {
     randomParameters[y * width * spp * SAMPLELENGTH + x * spp * SAMPLELENGTH + k * SAMPLELENGTH + offset] = para;
